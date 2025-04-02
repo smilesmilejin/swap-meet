23bb3c66ad703fce273db4e549e84873d6b2c4c2
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-02 15:16:21 -0400

    Refactor decor.py to use super()

--- swap_meet/decor.py
+++ swap_meet/decor.py
@@ -2,11 +2,10 @@ import uuid
 from swap_meet.item import Item
 
 class Decor(Item):
-    def __init__(self, id=None, width=None, length=None, condition=None):
-        self.id = uuid.uuid4().int if id is None else id
-        self.width = 0 if width is None else width
-        self.length = 0 if length is None else length
-        self.condition = 0 if condition is None else condition
+    def __init__(self, id=None, condition=None, width=None, length=None):
+        super().__init__(id, condition)
+        self.width = width if width is not None else 0 
+        self.length = length if length is not None else 0 
 
     def __str__(self):
         return f"An object of type Decor with id {self.id}. It takes up a {self.width} by {self.length} sized space."
