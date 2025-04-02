3ff2414b0a459a3f3d276e3862b15415afcb8863
Xinshuang Jin <jinxinshuang@gmail.com>
2025-04-02 13:40:22 -0400

    Refactor clothing.py to use super()

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,10 +2,9 @@ import uuid
 from swap_meet.item import Item
 
 class Clothing(Item):
-    def __init__(self, id=None, fabric=None, condition=None):
-        self.id = uuid.uuid4().int if id is None else id
-        self.fabric = "Unknown" if fabric is None else fabric
-        self.condition = 0 if condition is None else condition
+    def __init__(self, id=None, condition=None, fabric=None):
+        super().__init__(id, condition)
+        self.fabric = fabric if fabric is not None else "Unknown"
     def __str__(self):
         return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
 
